94665c4ebee88a5f1dda71c2100e19e9b0550c39
refat hasan <refat.aiub@hotmail.com>
2022-11-27 03:22:00 +0600

    commit_2
    update web route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/','welcome');
+Route::view('/about','about');
+Route::view('/gallery','gallery');
+Route::view('/blog','blog');
+Route::view('/academics','academics');
+Route::view('/curriculamvite','curriculamvite');
+Route::view('/publications','publications');
+Route::view('/projects','projects');
+Route::view('/books','books');
+Route::view('/recitation','recitation');
+Route::view('/initiatives','initiatives');
+Route::view('/dashboard','dashboard');
